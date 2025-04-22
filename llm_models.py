5b713c3e48032956d4f6b72f63b29b9c5643cf93
NaregM <nareg.mirzatuny@gmail.com>
2025-04-22 10:47:03 -0700

    update env

--- llm_models.py
+++ llm_models.py
@@ -1,10 +1,37 @@
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain.output_parsers import PydanticOutputParser
+
+from langchain_anthropic import ChatAnthropic
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
 
 llm_gemini = None
 llm_haiku = None
 llm_x = None
-llm_sonnet = None
 llm_gpt4o = None
 
+llm_sonnet = ChatAnthropic(
+    model="claude-3-7-sonnet-latest",
+    temperature=0,
+    max_tokens=1024,
+    timeout=None,
+    max_retries=2,
+    api_key = anthropic_api_key
+)
 
 models = {'gemini': llm_gemini,
                 'haiku': llm_haiku,

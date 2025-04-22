a7aa58485aca4b7f80651d8a8726742564db9520
NaregM <nareg.mirzatuny@gmail.com>
2025-04-21 21:54:09 -0700

    adding pairwise js div

--- helpers.py
+++ helpers.py
@@ -1,14 +1,43 @@
 import numpy as np
 import pandas as pd
 
+import torch
+from typing import List, Dict
+import numpy.typing as npt
+
 from collections import Counter
 from itertools import combinations
+
 from scipy.stats import entropy
 from transformers import AutoTokenizer, AutoModel
 from sklearn.cluster import KMeans
+
 import re
 
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 
+# Load the tokenizer and model (BERT-base-uncased)
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModel.from_pretrained("bert-base-uncased")
+embedding_layer = model.get_input_embeddings()
+
+# -----------------------------------------------------------------
+def tokenize(text: str) -> List[str]:
+    """
+    """
+    return tokenizer.tokenize(text)
+
+def get_token_embedding(token: str) -> npt.NDArray:
+    
+    """
+    """
+    token_id = tokenizer.convert_tokens_to_ids(token)
+    token_tensor = torch.tensor([token_id])
+    
+    with torch.no_grad():
+        
+        emb = embedding_layer(token_tensor)
+    
+    return emb.squeeze(0).numpy()
---
+++ pairwise_jsd.py
@@ -0,0 +1,17 @@
+###################################################################
+# For computing cluster probability distributions for each response
+# and calculate pairwise Jensen Shannon Divergence
+###################################################################
+import numpy as np
+from typing import List, Dict
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+
+def cluster_tokens(token_list: List[str], n_clusters: int = 30)\
+    -> Dict[str, int]:
+    """
+
+    """
+    return 
